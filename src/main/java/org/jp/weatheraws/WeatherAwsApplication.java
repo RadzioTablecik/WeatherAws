3ab4ac8dfe27d1d45f84594fce1b7a7e274aabd0
RadzioTablecik <kpiech70@gmail.com>
2026-05-01 22:58:51 +0200

    Initial commit

---
+++ src/main/java/org/jp/weatheraws/WeatherAwsApplication.java
@@ -0,0 +1,13 @@
+package org.jp.weatheraws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jp/weatheraws/WeatherAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jp.weatheraws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
