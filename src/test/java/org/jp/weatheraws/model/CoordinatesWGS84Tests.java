577f6da1a8753fed946101b0b084c51de0b564c5
RadzioTablecik <kpiech70@gmail.com>
2026-05-03 20:23:45 +0200

    Add CoordinatesWGS84 tests

---
+++ src/test/java/org/jp/weatheraws/model/CoordinatesWGS84Tests.java
@@ -0,0 +1,35 @@
+package org.jp.weatheraws.model;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("unit")
+class CoordinatesWGS84Tests {
+
+    @Test
+    void shouldCreateCoordinatesForValidInput() {
+        assertDoesNotThrow(() -> new CoordinatesWGS84(51.1, 17.03));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-90.1, 90.1})
+    void shouldThrowExceptionForInvalidLatitude(double invalidLat) {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                new CoordinatesWGS84(invalidLat, 0)
+        );
+        assertTrue(ex.getMessage().contains("Latitude"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-180.1, 180.1})
+    void shouldThrowExceptionForInvalidLongitude(double invalidLon) {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                new CoordinatesWGS84(0, invalidLon)
+        );
+        assertTrue(ex.getMessage().contains("Longitude"));
+    }
+}
