f776d993ba611f7c9eb0042864a3da9cf68c195c
RadzioTablecik <kpiech70@gmail.com>
2026-05-03 13:44:50 +0200

    Add api url to properties

--- src/main/java/org/jp/weatheraws/config/ClientConfig.java
+++ src/main/java/org/jp/weatheraws/config/ClientConfig.java
@@ -1,14 +1,24 @@
 package org.jp.weatheraws.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientConfig {
+    @Value("${clients.weather.openmeteo}")
+    private String openMeteoUrl;
 
     @Bean
-    public RestClient restClient() {
-        return RestClient.create();
+    public RestClient weatherRestClient() {
+        return RestClient.builder()
+                .baseUrl(openMeteoUrl)
+                .build();
     }
+//
+//    @Bean
+//    public RestClient geocodingClient() {
+//        return RestClient.create();
+//    }
 }
