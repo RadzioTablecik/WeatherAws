ffebcb70e5e803f78c2a36b0a5e2770a810b77b5
RadzioTablecik <kpiech70@gmail.com>
2026-05-03 14:20:40 +0200

    Add open meteo client

---
+++ src/main/java/org/jp/weatheraws/client/WeatherClient.java
@@ -0,0 +1,45 @@
+package org.jp.weatheraws.client;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.jp.weatheraws.dto.openmeteo.OpenMeteoResponseDto;
+import org.jp.weatheraws.model.CoordinatesWGS84;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.client.RestClient;
+
+@Component
+@Slf4j
+@Validated
+public class WeatherClient {
+    private final RestClient restClient;
+
+    public WeatherClient(@Qualifier("weatherRestClient") RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    /**
+     * Fetches current weather data from Open-Meteo API.
+     * Sample request:
+     * <a href="https://api.open-meteo.com/v1/forecast?latitude=51.1&longitude=17.0333&current=temperature_2m&timezone=auto"></a>
+     */
+    public OpenMeteoResponseDto fetchCurrentTemperature(@Valid CoordinatesWGS84 coordinates) {
+        log.debug("Fetching weather for lat: {}, lon: {}", coordinates.latitude(), coordinates.longitude());
+
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/forecast")
+                        .queryParam("latitude", coordinates.latitude())
+                        .queryParam("longitude", coordinates.longitude())
+                        .queryParam("current", "temperature_2m")
+                        .queryParam("timezone", "auto")
+                        .build())
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, (request, response) -> {
+                    throw new RuntimeException("OpenMeteo API error: " + response.getStatusCode());
+                })
+                .body(OpenMeteoResponseDto.class);
+    }
+}
